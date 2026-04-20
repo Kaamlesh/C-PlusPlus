f5aea27d89a743f3bd9491a39951f0f9569409ca
Kamlesh <kamlesh.stack@gmail.com>
2026-04-20 22:34:00 +0530

    arithmetic operators

---
+++ Basics/Operators/Arithmeticoperators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a = 10, b = 3;
+    cout << "ARITHMETIC OPERATORS IN C++" << endl;
+    cout << "----------------------------" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    cout << "Addition (a + b)       = " << a + b << endl;
+    cout << "Subtraction (a - b)    = " << a - b << endl;
+    cout << "Multiplication (a * b) = " << a * b << endl;
+    cout << "Division (a / b)       = " << a / b << endl;
+    cout << "Modulus (a % b)        = " << a % b << endl;
+    return 0;
+}
