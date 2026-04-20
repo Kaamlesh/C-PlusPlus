088d083f64242ff24f4e2bfaf000c43b726673d8
Kamlesh <kamlesh.stack@gmail.com>
2026-04-20 22:38:04 +0530

    relational operator

---
+++ Basics/Operators/RelationalOperators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a = 10, b = 20;
+    cout << "RELATIONAL OPERATORS IN C++" << endl;
+    cout << "---------------------------" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    cout << "a < b  = " << (a < b) << endl;
+    cout << "a <= b = " << (a <= b) << endl;
+    cout << "a > b  = " << (a > b) << endl;
+    cout << "a >= b = " << (a >= b) << endl;
+    cout << "a == b = " << (a == b) << endl;
+    cout << "a != b = " << (a != b) << endl;
+    return 0;
+}
