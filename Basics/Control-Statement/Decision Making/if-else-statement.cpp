821473e8ac5adc72f0b2c8a5fa6f654ceaaf9f07
Kamlesh <kamlesh.stack@gmail.com>
2026-04-21 21:55:14 +0530

    if else statement

---
+++ Basics/Control-Statement/Decision Making/if-else-statement.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int x = -5;
+    if (x >= 0) 
+    {
+        cout << "x is positive";
+    }
+    else 
+    {
+        cout << "x is negative";
+    }
+    return 0;
+}
